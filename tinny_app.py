51884d7e29e8216cac384cddf1985536c087f337
dinh ngo <ngodinh2339@gmail.com>
2025-02-27 15:42:26 +0700

    Them chuc nang dang ky

--- tinny_app.py
+++ tinny_app.py
@@ -1,10 +1,22 @@
-from flask import Flask, redirect, url_for, render_template
-
+from flask import Flask, redirect, url_for, render_template, request
+ 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('index.html', your_name = 'Dinh')
+    return render_template('index.html')
 
+@app.route('/login', methods = ['POST','GET'])
+def signin():
+    if request.method == 'POST':
+        user_email = request.form['email_acc']
+        user_pass = request.form['password_acc']
+        if user_email and user_pass:
+            return f'<h1> {user_email} </h1>'
+    return render_template('login.html')
+
+@app.route('/signup')
+def signup():
+    return render_template('logup.html')
 if __name__ == '__main__':
     app.run(debug=True)
